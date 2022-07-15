775c01a4c5b15ed5d0509f2711013b74596f4783
shilpi-code <jainshilpi080801@gmail.com>
2022-07-15 23:03:56 +0530

    added homepage

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import { AnimatePresence } from "framer-motion";
 
 export const App = () => {
   return (
-    <AnimatePresence>
+    <AnimatePresence exitBeforeEnter>
       <div className="w-screen h-auto flex flex-col bg-primary">
       <Header />
-      <main className='mt-24 p-8 w-full'>
+      <main className='mt-14 md:mt-20 px-4 md:px-16 py-4 w-full'>
         <Routes>
           <Route path='/*' element={<MainContainer />} />
-          <Route path='/create' element={<CreateContainer />} />
+          <Route path='/createItem' element={<CreateContainer />} />
         </Routes>
       </main>
     </div>
--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import HomeContainer from './HomeContainer';
 
 function MainContainer() {
   return (
-    <div>MainContainer</div>
+    <div className="w-full h-auto flex flex-col items-center justify-center ">
+      <HomeContainer />
+    </div>
   )
 }
 
