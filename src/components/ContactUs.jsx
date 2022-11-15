596037619e811966482553d63be5e72914e0145a
Shilpi Jain <jainshilpi080801@gmail.com>
2022-11-15 23:04:49 +0530

    added contact section

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -74,8 +74,7 @@ const ContactUs = () => {
           Contact Us
         </h2>
         <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
-          Got a technical issue? Want to send feedback about a beta feature?
-          Need details about our Business plan? Let us know.
+        Send us your query, we'll get back to you shortly.
         </p>
         <form action="#" class="space-y-8">
           <div>
@@ -94,19 +93,6 @@ const ContactUs = () => {
             />
           </div>
           <div>
-            <label
-              for="subject"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
-              Subject
-            </label>
-            <input
-              type="text"
-              id="subject"
-              class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-              placeholder="Let us know how we can help you"
-              required
-            />
           </div>
           <div class="sm:col-span-2">
             <label
