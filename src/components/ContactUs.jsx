ef4fa64952a7c025621456493b9231d623f4e392
Shilpi Jain <jainshilpi080801@gmail.com>
2022-11-15 21:23:33 +0530

    changed contact section

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -89,7 +89,7 @@ const ContactUs = () => {
               type="email"
               id="email"
               class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-              placeholder="name@flowbite.com"
+              placeholder="name@gmail.com"
               required
             />
           </div>
@@ -117,7 +117,7 @@ const ContactUs = () => {
             </label>
             <textarea
               id="message"
-              rows="6"
+              rows="4"
               class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
               placeholder="Leave a comment..."
             ></textarea>
